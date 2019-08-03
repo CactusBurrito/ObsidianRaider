df10a081209b90561d81bb0667e0e73f4af4986b
Klaudijus Miseckas <klaudmise@gmail.com>
2019-08-03 13:02:44 +0100

    Added basic command listener and handler for the plugin.

---
+++ src/main/java/net/cactusdev/obsidianraider/PluginCommands.java
@@ -0,0 +1,14 @@
+package net.cactusdev.obsidianraider;
+
+/**
+ * All commands that this plugin can detect and handle.
+ */
+public class PluginCommands
+{
+
+	/**
+	 * Fully reloads the plugin.
+	 */
+	public static final String RELOAD = "reload";
+
+}
--- src/main/java/net/cactusdev/obsidianraider/listeners/CommandHandler.java
+++ src/main/java/net/cactusdev/obsidianraider/listeners/CommandHandler.java
@@ -1,4 +1,35 @@
 package net.cactusdev.obsidianraider.listeners;
 
-public class CommandHandler
-{}
+import net.cactusdev.obsidianraider.ObsidianRaiderMain;
+import net.cactusdev.obsidianraider.PluginCommands;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+
+import static org.bukkit.Bukkit.getServer;
+
+/**
+ * Class responsible for listening and handling user commands.
+ */
+public class CommandHandler implements Listener
+{
+
+	public CommandHandler()
+	{
+		getServer().getPluginManager().registerEvents(this, ObsidianRaiderMain.GetInstance());
+	}
+
+	@EventHandler(priority = EventPriority.MONITOR)
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
+	{
+		if (cmd.getName().equalsIgnoreCase(PluginCommands.RELOAD))
+		{
+			return true;
+		}
+
+
+		return false;
+	}
+}
